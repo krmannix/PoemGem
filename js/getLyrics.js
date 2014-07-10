b4936d0f0aca01b49b8f080472f633996efa5c0e
Kevin Mannix <kmannix@bu.edu>
2014-07-10 19:20:40 -0400

    Adding capability for Parse database function calls. Updated web scraping function to return and obtain lyrics

--- js/getLyrics.js
+++ js/getLyrics.js
@@ -1,64 +1,84 @@
-
-// Find artist will break down the artist name into a query and plug it into the options variable
-// This should return a list of potential artists as a response
-function findArtist(artistName) {
-
-	console.log("Finding artist " + artistName);
-
-
-	// This function will execute on the response being received
-	callback = function(response) {
-		var body = '';
-		response.on("data", function(chunk) {
-			body += chunk;
-		});
-		response.on("end", function() {
-			console.log(body);
-			findArtistUrl(response);
-		});
-		
-	}
-	http.request(options, callback).end();
-}
-
-// findArtistUrl() will check to see if the response is successful 
-// - if so, we will take the response and find the link to the first artist's page
-function findArtistUrl(response) {
-
-	// See if response is JSON
-	try {
-		// 
-		switch (response.headers.status) {
-			case "301 Moved Permanently":
-				// This means we should use the location they give us, because the link has moved
-				var newLocation = response.headers.location;
-				options = {
-					host: newLocation.split("//")[1].split("/")[0],
-					path: "/" + newLocation.split("//")[1].split("/")[1]
-				}
-				findArtist("No name");
-				break;
-			case "200 OK":
-				console.log("Successful search");
-				//console.log(response);
-				break;
-		}
-	} catch(err) {
-		console.log("Response is not JSON. Need assistance.")
-	}
-}
-
-
-var http = require('http');
+// Initialize html parser and HTTP request modules
 var request = require('request');
 var cheerio = require('cheerio');
 
-/*request('http://rapgenius.com/search?q=lil+wayne', function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    console.log(body); // Print the google web page.
-  }
-});*/
+// Return body of artist search from artist name from Rap Genius
+function findArtist(artistName) {
+	request('http://rapgenius.com/search?q=' + formatArtistName(artistName), function (error, response, body) {
+		console.log("Link: http://rapgenius.com/search?q=" + formatArtistName(artistName));
+	  if (!error && response.statusCode == 200) {
 
+	    findArtistLinkFromHTML(body); // Send the body to be parsed
+	  } else {
+	  	console.log("Something went wrong");
+	  }
+	});
+}
+
+// Get links of songs for artist
+function findArtistLinkFromHTML(body) {
+	var artistLinks = [];
+	$ = cheerio.load(body);
+	var linksObjects = $(".song_link");
+	var links = [];
+	//for (var i = 0; i < linksObjects.length; i++) {
+		goToLink(linksObjects[0].attribs.href);
+	//}
+}
+
+// Go to link for song to scrape lyrics
+function goToLink(link) {
+	request(link, function(error, response, body) {
+		if (!error && response.statusCode == 200) {
+			scrapeLyrics(body); // Send the body to be parsed
+		} else {
+			console.log("Something went wrong accessing song link");
+		}
+	});
+}
+
+// Grab lyrics for a song 
+function scrapeLyrics(body) {
+	$ = cheerio.load(body);
+	// lines will hold all applicable 
+	var lines = [];
+	var lyricObject = $(".lyrics > p").find("a");
+
+	// Parse through each lyric and it's children
+	for (var j = 0; j < lyricObject.length; j++) {
+		for (var k = 0; k < lyricObject[j].children.length; k++) {
+			if (lyricObject[j].children[k].data != undefined ) {
+
+				// Remove extraneous characters and ensure that line is not blank, then add to array
+				var line = lyricObject[j].children[k].data.replace(/\[.*\]|\"|\,|\r?\n|\r/g, "");
+				if (!(line === "")) lines.push(line);
+			}
+		}
+	}
+	createMap(lines);
+}
+
+// This function will return an artists name down to a form in which urls can take
+function formatArtistName(artistName) {
+	return artistName.trim().replace(/'/g, "%27").replace(/\s{2,}/g, "+").toLowerCase();
+}
+
+function createMap(linkArray) {
+
+}
+
+// Send a request to DB to check if artist has already been serviced. This is so we don't unfairly
+// favor certain artists if their songs have already been uploaded
+function checkForNewArtist(artistName) {
+	
+}
+
+
+/*===================
+ *
+ *	Main
+ *
+ *===================*/
 
 findArtist("Lil Wayne");
 
