e676f2a81741e9c8de45cb4479d047b16cf4cf52
unknown <UMANNK1@BOMASWL-7SKQRY1.PEROOT.com>
2014-07-11 11:14:29 -0400

    Converted JSON array to full JSON object for ease of transportation between modules.

--- js/getLyrics.js
+++ js/getLyrics.js
@@ -4,6 +4,9 @@ var cheerio = require('cheerio');
 var Parse = require('parse').Parse;
 Parse.initialize("xVGwbfMCJHMeWgDDF8F7kjl82tqI7nISHMEkST9p", "dhKnIYqkzvCFC7mZ5qCCLFCqJNDACWE3UXph7tM4");
 
+// Set global variables to help "block" non-blocking calls
+
+
 function runArtistScript(artistName) {
 	checkForNewArtist(formatArtistName(artistName));
 }
@@ -40,17 +43,21 @@ function findArtist(artistName) {
 
 // Get links of songs for artist
 function findArtistLinkFromHTML(body) {
-	var artistLinks = [];
+	var links = [];
 	$ = cheerio.load(body);
 	var linksObjects = $(".song_link");
-	var links = [];
+	for (var i = 0; i < linksObjects.length; i++) {
+		links.push(linksObjects[i].attribs.href);
+	}
 	//for (var i = 0; i < linksObjects.length; i++) {
 		goToLink(linksObjects[0].attribs.href);
+		console.log("Done with this link");
 	//}
 }
 
 // Go to link for song to scrape lyrics
 function goToLink(link) {
+	console.log("Go to Link");
 	request(link, function(error, response, body) {
 		if (!error && response.statusCode == 200) {
 			scrapeLyrics(body); // Send the body to be parsed
@@ -62,6 +69,7 @@ function goToLink(link) {
 
 // Grab lyrics for a song 
 function scrapeLyrics(body) {
+	console.log("Scrape lyrics");
 	$ = cheerio.load(body);
 	// lines will hold all applicable 
 	var lines = [];
@@ -73,7 +81,7 @@ function scrapeLyrics(body) {
 			if (lyricObject[j].children[k].data != undefined ) {
 
 				// Remove extraneous characters and ensure that line is not blank, then add to array
-				var line = lyricObject[j].children[k].data.replace(/\[.*\]|\"|\,|\r?\n|\r/g, "");
+				var line = lyricObject[j].children[k].data.replace(/\[.*\]|\"|\,|\r?\n|\r/g, "").toLowerCase();
 				if (!(line === "")) lines.push(line);
 			}
 		}
@@ -89,33 +97,34 @@ function formatArtistName(artistName) {
 // This function will take array of lyric lines and create hashmaps for each word with a counter in JSON
 // Final output will be JSON array
 function createMapOneDegree(linkArray) {
+	console.log("Creating map");
 	// Create empty array to hold all JSON data
-	var JSONwords = [];
+	var JSONwords = {"allWords" : []};
 
 	// Go through each lyric and break each word apart into seperate indexes
 	for (var i = 0; i < linkArray.length; i++) {
 		var wordsInLine = linkArray[i].split(" ");
 		for (var k = 0; k < wordsInLine.length - 1; k++) { // Go through each word in thisline, except for the last one (since it has no next word)
 
-			if (JSONwords.length == 0) { // Initialize wordsInLine with first word
-				JSONwords.push({"currentWord" : wordsInLine[k], "nextWords": []});
+			if (JSONwords.allWords.length == 0) { // Initialize wordsInLine with first word
+				JSONwords.allWords.push({"currentWord" : wordsInLine[k], "nextWords": []});
 			} 
 
 			var doesCurrentWordAlreadyExist = false;
-			for (var j = 0; j < JSONwords.length; j++) { // Go through each word already seen in lyrics 
+			for (var j = 0; j < JSONwords.allWords.length; j++) { // Go through each word already seen in lyrics 
 
-				if (JSONwords[j].currentWord === wordsInLine[k]) { // Look to see if we already have that word stored in the JSONwords array
+				if (JSONwords.allWords[j].currentWord === wordsInLine[k]) { // Look to see if we already have that word stored in the JSONwords array
 					doesCurrentWordAlreadyExist = true;
 					var doesNextWordAlreadyExist = false; // Now that the word already exists in JSONwords, see if the next word already exists as well
-					for (next in JSONwords[j].nextWords) {
-						if (JSONwords[j].nextWords[next].word === wordsInLine[k+1]) {
+					for (next in JSONwords.allWords[j].nextWords) {
+						if (JSONwords.allWords[j].nextWords[next].word === wordsInLine[k+1]) {
 							doesNextWordAlreadyExist = true; 
-							JSONwords[j].nextWords[next].count = Number(JSONwords[j].nextWords[next].count) + 1; // If already exists, increment number
+							JSONwords.allWords[j].nextWords[next].count = Number(JSONwords.allWords[j].nextWords[next].count) + 1; // If already exists, increment number
 							break;
 						}
 					}
 					if (!doesNextWordAlreadyExist) { // If this is a new next word for the current word in JSONwords, initialize and push it
-						JSONwords[j].nextWords.push({ "word": wordsInLine[k+1], "count": 1});
+						JSONwords.allWords[j].nextWords.push({ "word": wordsInLine[k+1], "count": 1});
 						// push to JSONwords.nextWords
 					}
 					break;
@@ -125,9 +134,12 @@ function createMapOneDegree(linkArray) {
 			if (!doesCurrentWordAlreadyExist) {
 				var nextWords = [];
 				nextWords.push({"word": wordsInLine[k+1], "count" : 1});
-				JSONwords.push({"currentWord" : wordsInLine[k], "nextWords": nextWords});
+				JSONwords.allWords.push({"currentWord" : wordsInLine[k], "nextWords": nextWords});
 			}
 		}
+	}
+	for (var q = 0; q <3; q++) {
+		console.log(JSON.stringify(JSONwords.allWords[q], null, 2));
 	}
 }
 
